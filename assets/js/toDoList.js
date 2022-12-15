af6de453715263b484fd3a7ea413330488ba3bf7
Vallerga <antoniovallerga85@gmail.com>
2022-12-15 16:53:48 +0100

    incomplete work

---
+++ assets/js/toDoList.js
@@ -0,0 +1,9 @@
+
+const liGenerator = document.querySelector("button");
+liGenerator.onclick = function (event) {
+    const toDoPoint = document.createElement("li");
+    const transferText = document.querySelector("input");
+    transferText.innerText.inputvalue;
+    toDoPoint.appendChild("ul");
+    
+}
