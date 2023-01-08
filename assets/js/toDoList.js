2d55f5bee3ff11e139cb44e73f5e6d12d1fb9698
Vallerga <antoniovallerga85@gmail.com>
2023-01-08 18:52:37 +0100

    esercizi svolti

--- assets/js/toDoList.js
+++ assets/js/toDoList.js
@@ -1,9 +1,33 @@
+const liGenerator = (e) => {
+  const selInputText = document.querySelector("input");
+  const selUl = document.querySelector("ul");
+  const createLi = document.createElement("li");
+  const createSpan = document.createElement("span");
+  (createLi.classList += "newLi");
+  createSpan.innerText = selInputText.value;
+  createLi.appendChild(createSpan);
+  const createButton = document.createElement("button");
+  createButton.innerText = "Clear";
+  selUl.appendChild(createLi);
+  createLi.appendChild(createButton);
+  createLi.onclick = taskComplete;
+  createButton.onclick = removeLi;
+};
 
-const liGenerator = document.querySelector("button");
-liGenerator.onclick = function (event) {
-    const toDoPoint = document.createElement("li");
-    const transferText = document.querySelector("input");
-    transferText.innerText.inputvalue;
-    toDoPoint.appendChild("ul");
-    
+const taskComplete = (event) => {
+  event.currentTarget.classList += " complete";
+};
+
+const removeLi = (event) => {
+  event.target.parentNode.remove();
+};
+
+const sortTask = (event) => {
+    const selSortedTask = document.querySelector(".sortedTast");
+    const selUnSortedTask = document.querySelector(".unsortedTast");
+    selComplete = document.getElementsByClassName("newLi");
+    let liArray = Array.from(selComplete)
+    console.log(selComplete)
+    console.log(liArray)
+    liArray.forEach((element) => element.className.includes(" complete") ? selSortedTask.appendChild(element) : selUnSortedTask.appendChild(element));
 }
